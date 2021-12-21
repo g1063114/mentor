25e0d69dbe6018219eccb41b10e07419ead2d557
jinho <g1063114@naver.com>
2021-12-21 21:41:42 +0900

    member entity create...ing

---
+++ src/main/kotlin/com/mentoring/mentor/member/entity/Member.kt
@@ -0,0 +1,13 @@
+package com.mentoring.mentor.member.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id : Long? = null
+}
--- src/main/kotlin/com/mentoring/mentor/testcontroller/HelloController.kt
+++ src/main/kotlin/com/mentoring/mentor/testcontroller/HelloController.kt
@@ -1,4 +1,4 @@
-package com.mentoring.mentor.controller
+package com.mentoring.mentor.testcontroller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
